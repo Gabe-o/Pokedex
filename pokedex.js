7a31f7ad837f3b467b8500a90b665dae1715a891
golivott <golivott@uwo.ca>
2022-09-21 11:12:37 -0400

    Improved search by ID for inputs that include leading zeros

--- pokedex.js
+++ pokedex.js
@@ -14,26 +14,26 @@ class Pokemon {
 
 // Declared all Pokemon
 var pokeArr = [
-    new Pokemon(1,"Bulbasaur","Grass / Poison Type from Gen 1 with a Rarity of 4"),
-    new Pokemon(2,"Ivysaur","Grass / Poison Type from Gen 1 with a Rarity of 5"),
-    new Pokemon(3,"Venusaur","Grass / Poison Type from Gen 1 with a Rarity of 6"),
-    new Pokemon(4,"Charmander","Fire Type from Gen 1 with a Rarity of 4"),
-    new Pokemon(5,"Charmeleon","Fire Type from Gen 1 with a Rarity of 5"),
-    new Pokemon(6,"Charizard","Fire / Flying Type from Gen 1 with a Rarity of 6"),
-    new Pokemon(7,"Squirtle","Water Type from Gen 1 with a Rarity of 4"),
-    new Pokemon(8,"Wartortle","Water Type from Gen 1 with a Rarity of 5"),
-    new Pokemon(9,"Blastoise","Water Type from Gen 1 with a Rarity of 6"),
-    new Pokemon(10,"Caterpie","Bug Type from Gen 1 with a Rarity of 1"),
-    new Pokemon(11,"Metapod","Bug Type from Gen 1 with a Rarity of 3"),
-    new Pokemon(12,"Butterfree","Bug / Flying Type from Gen 1 with a Rarity of 4"),
-    new Pokemon(13,"Weedle","Bug / Poison Type from Gen 1 with a Rarity of 1"),
-    new Pokemon(14,"Kakuna","Bug / Poison Type from Gen 1 with a Rarity of 3"),
-    new Pokemon(15,"Beedrill","Bug / Poison Type from Gen 1 with a Rarity of 4"),
-    new Pokemon(16,"Pidgey","Normal / Flying Type from Gen 1 with a Rarity of 1"),
-    new Pokemon(17,"Pidgeotto","Normal / Flying Type from Gen 1 with a Rarity of 3"),
-    new Pokemon(18,"Pidgeot","Normal / Flying Type from Gen 1 with a Rarity of 4"),
-    new Pokemon(19,"Rattata","Normal Type from Gen 1 with a Rarity of 1"),
-    new Pokemon(20,"Raticate","Normal Type from Gen 1 with a Rarity of 4"),
+    new Pokemon("001","Bulbasaur","Grass / Poison Type from Gen 1 with a Rarity of 4"),
+    new Pokemon("002","Ivysaur","Grass / Poison Type from Gen 1 with a Rarity of 5"),
+    new Pokemon("003","Venusaur","Grass / Poison Type from Gen 1 with a Rarity of 6"),
+    new Pokemon("004","Charmander","Fire Type from Gen 1 with a Rarity of 4"),
+    new Pokemon("005","Charmeleon","Fire Type from Gen 1 with a Rarity of 5"),
+    new Pokemon("006","Charizard","Fire / Flying Type from Gen 1 with a Rarity of 6"),
+    new Pokemon("007","Squirtle","Water Type from Gen 1 with a Rarity of 4"),
+    new Pokemon("008","Wartortle","Water Type from Gen 1 with a Rarity of 5"),
+    new Pokemon("009","Blastoise","Water Type from Gen 1 with a Rarity of 6"),
+    new Pokemon("010","Caterpie","Bug Type from Gen 1 with a Rarity of 1"),
+    new Pokemon("011","Metapod","Bug Type from Gen 1 with a Rarity of 3"),
+    new Pokemon("012","Butterfree","Bug / Flying Type from Gen 1 with a Rarity of 4"),
+    new Pokemon("013","Weedle","Bug / Poison Type from Gen 1 with a Rarity of 1"),
+    new Pokemon("014","Kakuna","Bug / Poison Type from Gen 1 with a Rarity of 3"),
+    new Pokemon("015","Beedrill","Bug / Poison Type from Gen 1 with a Rarity of 4"),
+    new Pokemon("016","Pidgey","Normal / Flying Type from Gen 1 with a Rarity of 1"),
+    new Pokemon("017","Pidgeotto","Normal / Flying Type from Gen 1 with a Rarity of 3"),
+    new Pokemon("018","Pidgeot","Normal / Flying Type from Gen 1 with a Rarity of 4"),
+    new Pokemon("019","Rattata","Normal Type from Gen 1 with a Rarity of 1"),
+    new Pokemon("020","Raticate","Normal Type from Gen 1 with a Rarity of 4"),
 ];
 
 //Function for searching pokeArr by ID
@@ -41,15 +41,15 @@ function searchByID() {
     //Gets user input
     var input = document.getElementById("idSearch").value;
 
-    //Input Validation
-    if(input > 0 && input <= 20) {
+    //Input Validation by parseing string input to integer to account for inputs that include leading zeros
+    if(parseInt(input.toString()) > 0 && parseInt(input.toString()) <= 20) {
 
         //Finds the first 5 results that include the input
         var resultsString = "Results: \n";
         var resultsFound = 0;
         for(var i = 0; i < pokeArr.length && resultsFound < 5; i++) {
             
-            if(pokeArr[i].id.toString().includes(input)) {
+            if(pokeArr[i].id.toString().includes(input.toString())) {
                 resultsString += pokeArr[i].toString()+"\n"; // appending found result to the string
                 resultsFound++;
             }
@@ -74,7 +74,7 @@ function searchByName() {
     //Gets user input
     var input = document.getElementById("nameSearch").value;
 
-    //Input Validation
+    //Input Validation by checking if the input is less then or equal to 20 characters and only uses the characters a-z or A-Z
     if(input.length <= 20  && /^[a-zA-Z]/.test(input)) {
 
         //Finds the first 5 results that include the input
